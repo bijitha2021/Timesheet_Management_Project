350ac60c88966b2c163698928ae3ad65cdf0cb89
bijitha2021 <bijitha@gmail.com>
2022-02-07 21:15:02 -0600

    added list and display feature

--- Timesheet_Management_Project/timesheetManagement/userManagement/views.py
+++ Timesheet_Management_Project/timesheetManagement/userManagement/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from userManagement.models import User
+from django.views.generic import ListView, DetailView, CreateView
+
+
+
+
+class UserList(ListView):
+    model=User
+    context_object_name='Users'
+    template_name='userManagement/user_list.html'
+
+
+class UserDetail(DetailView):
+    model = User
+    context_object_name = 'Users'
+    template_name = 'userManagement/user_detail.html'
+
+
+
