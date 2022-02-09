afc0572c8624bd4f3d617e3a42d8375672f39ff1
kanchan-pat <82234347+kanchan-pat@users.noreply.github.com>
2022-02-09 08:05:46 -0500

    Update models.py

--- Timesheet_Management_Project/timesheetManagement/userManagement/models.py
+++ Timesheet_Management_Project/timesheetManagement/userManagement/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-class User(models.Model):
+class User(models.Model): # naming your model User as well, there will be conflict between auth_user and userManagenet_user, since django has some built in functionality to implement authentication. This may create problem in few activitities. I suggest name it different
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     description = models.CharField(max_length=100)
     image=models.ImageField(upload_to='userManagement/',null=True, blank=True)
